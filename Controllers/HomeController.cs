e98efebb6dcfd47ad473bfd339fd2115ecf67500
dpjensen22 <dpjensen22@gmail.com>
2022-04-04 15:54:51 -0600

    index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,10 @@ namespace Collision_App.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
@@ -23,15 +22,5 @@ namespace Collision_App.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }

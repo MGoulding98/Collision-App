2a016f2e7082ad362365c527821f6b99d9f88feb
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-04-04 19:23:49 -0600

    Incorrect DB connection method

---
+++ Models/RDSContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Collision_App.Models
+{
+    public class RDSContext : DbContext
+    {
+        public RDSContext(DbContextOptions<RDSContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Crash> Crashes { get; set; }
+        public DbSet<Severity> Severities { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Collision_App.Models;
 
 namespace Collision_App
 {
@@ -24,6 +26,11 @@ namespace Collision_App
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<RDSContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:RDSConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

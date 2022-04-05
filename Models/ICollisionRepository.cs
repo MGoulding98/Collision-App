38c1454e1f0c928b97201c72c7eff94e390ebe8b
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-04-04 21:13:08 -0600

    updated names and repos

--- Models/ICollisionRepository.cs
+++ Models/ICollisionRepository.cs
@@ -2,10 +2,14 @@
 using System.Linq;
 using ProjectDriveSafe.Models;
 
-namespace Collision_App.Models
+namespace ProjectDriveSafe.Models
 {
     public class ICollisionRepository
     {
         IQueryable<Crash> Crashes { get; }
+
+        public void SaveCollision(Crash c);
+        public void CreateCollision(Crash c);
+        public void DeleteCollision(Crash c);
     }
 }

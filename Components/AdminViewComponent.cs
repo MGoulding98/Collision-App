249a8bf901a9c7c90bf358de45106cbdddc071a0
bHicks2112 <79944750+bHicks2112@users.noreply.github.com>
2022-04-05 21:57:10 -0600

    AdminViewComponent

---
+++ Components/AdminViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using ProjectDriveSafe.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectDriveSafe.Components
+{
+    public class AdminViewComponent : ViewComponent
+    {
+        private ICollisionRepository repo { get; set; }
+
+        public AdminViewComponent(ICollisionRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedTeam = RouteData?.Values["county"];
+
+            var Counties = repo.Crashes
+                .Select(x => x.COUNTY_NAME)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(Counties);
+        }
+    }
+}
+
